41bc1824012b0fbd6582a4ee4ad0c3c1656d8887
satyam singh <143834423+Satyam93186@users.noreply.github.com>
2026-03-26 22:55:58 +0530

    Time: 2 ms (34.45%), Space: 42.8 MB (53.87%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int combinationSum4(int[] a, int t) {
+        int[] dp = new int[t+1];
+        dp[0] = 1;
+
+        for(int i=1;i<=t;i++){
+            for(int x : a){
+                if(i >= x)
+                    dp[i] += dp[i - x];
+            }
+        }
+
+        return dp[t];
+    }
+}
